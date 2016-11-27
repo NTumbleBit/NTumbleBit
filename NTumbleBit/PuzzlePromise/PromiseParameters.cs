8b2699172cbc7f860ce15ef6520c4b112ae9c703
NicolasDorier <nicolas.dorier@gmail.com>
2016-11-28 01:21:04 +0900

    Promise

--- NTumbleBit/PuzzlePromise/PromiseParameters.cs
+++ NTumbleBit/PuzzlePromise/PromiseParameters.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NBitcoin;
+using NBitcoin.Crypto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,17 +21,28 @@ namespace NTumbleBit.PuzzlePromise
 			set;
 		}
 
+		public uint256 FakeFormat
+		{
+			get; set;
+		}
+
 		public PromiseParameters()
 		{
 			FakeTransactionCount = 42;
 			RealTransactionCount = 42;
+			FakeFormat = Hashes.Hash256(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16 });
 		}
 
-		public PromiseParameters(RsaKey serverKey):this()
+		public PromiseParameters(RsaKey serverKey) : this()
 		{
 			if(serverKey == null)
 				throw new ArgumentNullException("serverKey");
 			ServerKey = serverKey;
+		}
+
+		public int GetTotalTransactionsCount()
+		{
+			return FakeTransactionCount + RealTransactionCount;
 		}
 
 		public RsaKey ServerKey
