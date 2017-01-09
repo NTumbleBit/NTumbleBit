e55284d6647aa0ae8d5b8d61a362ee3a88e3348f
nopara73 <adam.ficsor73@gmail.com>
2017-01-09 15:40:04 +0800

    Throw exception if patient delete times out

--- NTumbleBit/Utils.cs
+++ NTumbleBit/Utils.cs
@@ -118,7 +118,9 @@ namespace NTumbleBit
 					return;  // good!
 				}
 				catch (IOException)
-				{ 
+				{
+					if (gnomes == magicDust)
+						throw;
 					// System.IO.IOException: The directory is not empty
 					System.Diagnostics.Debug.WriteLine("Gnomes prevent deletion of {0}! Applying magic dust, attempt #{1}.", destinationDir, gnomes);
 
@@ -128,16 +130,14 @@ namespace NTumbleBit
 				}
 				catch (UnauthorizedAccessException)
 				{
+					if (gnomes == magicDust)
+						throw;
 					// Wait, maybe another software make us authorized a little later
 					System.Diagnostics.Debug.WriteLine("Gnomes prevent deletion of {0}! Applying magic dust, attempt #{1}.", destinationDir, gnomes);
 
 					// see http://stackoverflow.com/questions/329355/cannot-delete-directory-with-directory-deletepath-true for more magic
 					Thread.Sleep(100);
 					continue;
-				}
-				catch (Exception)
-				{
-					throw;
 				}
 				return;
 			}
