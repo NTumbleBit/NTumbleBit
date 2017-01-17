6fbcd687f539402b67ef4f8521308c739b40db6d
Daniel Gould <dan@dannygould.com>
2017-01-17 12:03:01 -0500

    fix wait until first poll to populate state tables

--- NTumbleBit.TumblerServer/wwwroot/main.js
+++ NTumbleBit.TumblerServer/wwwroot/main.js
@@ -3,30 +3,30 @@ const solverServerSessionStatesUri = "http://localhost:5000/api/v1/tumblers/0/So
 const promiseServerSessionStatesUri = "http://localhost:5000/api/v1/tumblers/0/PromiseServerSessionStates";
 
 const solverServerSessionStates = {
-  WaitingEscrow: 1,
-  WaitingPuzzles: 2,
-  WaitingRevelation: 3,
-  WaitingBlindFactor: 4,
-  WaitingFulfillment: 5,
-  Completed: 6
-}
+  "WaitingEscrow": 1,
+  "WaitingPuzzles": 2,
+  "WaitingRevelation": 3,
+  "WaitingBlindFactor": 4,
+  "WaitingFulfillment": 5,
+  "Completed": 6
+};
 
 const promiseServerSessionStates = {
-  WaitingEscrow: 1,
-  WaitingHashes: 2,
-  WaitingRevelation: 3,
-  Completed: 4
-}
+  "WaitingEscrow": 1,
+  "WaitingHashes": 2,
+  "WaitingRevelation": 3,
+  "Completed": 4
+};
 
 let solvers = [];
 let promises = [];
 
-const solversDiv = document.querySelector('.solvers ul');
-const promisesDiv = document.querySelector('.promises ul');
+const inputsDiv = document.querySelector('.inputs ul');
+const outputsDiv = document.querySelector('.outputs ul');
 
-(function () {
+(function poll() {
   fetchEndpoints();
-  setTimeout(fetchEndpoints, 1000);
+  setTimeout(poll, 10000);
 })();
 
 function handleErrors(response) {
@@ -37,40 +37,39 @@ function handleErrors(response) {
 }
 
 function fetchEndpoints() {
-  console.log('fetch');
   fetch(solverServerSessionStatesUri)
-  .then(handleErrors)
-  .then((res) => res.json())
-  .then((data) => solvers = data);
+    .then(handleErrors)
+    .then((res) => res.json())
+    .then((solvers) => updateInputs(solvers));
 
   fetch(promiseServerSessionStatesUri)
     .then(handleErrors)
     .then((res) => res.json())
-    .then((data) => promises = data);
+    .then((promises) => updateOutputs(promises));
 
-  updateDOM();
 }
 
-
-
-function updateDOM() {
-  solversDiv.innerHTML = solvers.map(solver =>
+function updateInputs(solvers) {
+  inputsDiv.innerHTML = solvers.map(solver =>
     genSolverComponent(solver)
   ).join('');
+}
 
-  promisesDiv.innerHTML = promises.map(promise =>
+function updateOutputs(promises) {
+  outputsDiv.innerHTML = promises.map(promise =>
     genPromiseComponent(promise)
   ).join('');
 }
+
 
 function genSolverComponent(solver) {
   const status = solver.status;
   const id = solver.escrowedCoin.scriptPubKey;
   return `
-    <li>
-      <p>${id}</p>
-      <label>Progress: ${status}</label>
-      <progress value="1" max="6">
+    <li class="solver">
+      <p>scriptPubKey: ${id}</p>
+      <label>status: ${status}</label>
+      <progress value="${solverServerSessionStates[status]}" max="6">
     </li>
   `;
 }
@@ -79,10 +78,10 @@ function genPromiseComponent(promise) {
   const status = promise.status;
   const id = promise.escrowedCoin.scriptPubKey;
   return `
-    <li>
-      <p>${id}</p>
-      <label>Progress: ${status}</label>
-      <progress value="1" max="4">
+    <li class="promise">
+      <p>scriptPubKey: ${id}</p>
+      <label>status: ${status}</label>
+      <progress value="${promiseServerSessionStates[status]}" max="4">
     </li>
   `;
 }
