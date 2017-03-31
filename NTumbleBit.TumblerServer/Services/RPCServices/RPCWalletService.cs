be2213c06b88c64dd4f25e5bef1e799484bf7789
NicolasDorier <nicolas.dorier@gmail.com>
2017-03-31 21:17:21 +0900

    Fix transaction version number was silently modified by NBitcoin in FundTransaction

--- NTumbleBit.TumblerServer/Services/RPCServices/RPCWalletService.cs
+++ NTumbleBit.TumblerServer/Services/RPCServices/RPCWalletService.cs
@@ -6,6 +6,7 @@ using NBitcoin;
 using NBitcoin.RPC;
 using Newtonsoft.Json.Linq;
 using NTumbleBit.PuzzlePromise;
+using NBitcoin.DataEncoders;
 
 #if !CLIENT
 namespace NTumbleBit.TumblerServer.Services.RPCServices
@@ -51,7 +52,9 @@ namespace NTumbleBit.Client.Tumbler.Services.RPCServices
 			tx.Outputs.Add(txOut);
 
 			var changeAddress = BitcoinAddress.Create(_RPCClient.SendCommand("getrawchangeaddress").ResultString, _RPCClient.Network);
-			var result = _RPCClient.SendCommandNoThrows("fundrawtransaction", tx.ToHex(), new JObject
+
+
+			var result = _RPCClient.SendCommandNoThrows("fundrawtransaction", ToHex(tx), new JObject
 			{
 				new JProperty("lockUnspents", true),
 				new JProperty("feeRate", feeRate.GetFee(1000).ToDecimal(MoneyUnit.BTC)),
@@ -70,6 +73,16 @@ namespace NTumbleBit.Client.Tumbler.Services.RPCServices
 			jobj = (JObject)result.Result;
 			hex = jobj["hex"].Value<string>();
 			return new Transaction(hex);
+		}
+
+		//NBitcoin internally put a bit in the version number to make difference between transaction without input and transaction with witness.
+		private string ToHex(Transaction tx)
+		{
+			// if there is inputs, then it can't be confusing
+			if(tx.Inputs.Count > 0)
+				return tx.ToHex();
+			// if there is, do this ACK so that NBitcoin does not change the version number
+			return Encoders.Hex.EncodeData(tx.ToBytes(NBitcoin.Protocol.ProtocolVersion.WITNESS_VERSION - 1));
 		}
 	}
 }
