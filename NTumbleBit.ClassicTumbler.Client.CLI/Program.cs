0d2e34786b3c92b8a290d975cb46a945bab79543
NicolasDorier <nicolas.dorier@gmail.com>
2017-09-10 11:19:17 +0900

    Add redeemescrows debug command for testnet storms

--- NTumbleBit.ClassicTumbler.Client.CLI/Program.cs
+++ NTumbleBit.ClassicTumbler.Client.CLI/Program.cs
@@ -11,6 +11,8 @@ using NTumbleBit.ClassicTumbler;
 using NTumbleBit.Configuration;
 using NTumbleBit.ClassicTumbler.Client;
 using NTumbleBit.ClassicTumbler.CLI;
+using System.Threading.Tasks;
+using NTumbleBit.Services.RPC;
 
 namespace NTumbleBit.ClassicTumbler.Client.CLI
 {
@@ -24,11 +26,12 @@ namespace NTumbleBit.ClassicTumbler.Client.CLI
 		{
 			var argsConf = new TextFileConfiguration(args);
 			var debug = argsConf.GetOrDefault<bool>("debug", false);
+			var redeemEscrows = argsConf.GetOrDefault<bool>("redeemescrows", false); ;
+
 			ConsoleLoggerProcessor loggerProcessor = new ConsoleLoggerProcessor();
 			Logs.Configure(new FuncLoggerFactory(i => new CustomerConsoleLogger(i, Logs.SupportDebug(debug), false, loggerProcessor)));
 			using(var interactive = new Interactive())
 			{
-
 				try
 				{
 					var config = new TumblerClientConfiguration();
@@ -36,6 +39,11 @@ namespace NTumbleBit.ClassicTumbler.Client.CLI
 
 					var runtime = TumblerClientRuntime.FromConfiguration(config, new TextWriterClientInteraction(Console.Out, Console.In));
 					interactive.Runtime = new ClientInteractiveRuntime(runtime);
+
+					if(redeemEscrows)
+					{
+						RedeemEscrows(runtime).GetAwaiter().GetResult();
+					}
 
 
 					var broadcaster = runtime.CreateBroadcasterJob();
@@ -68,6 +76,27 @@ namespace NTumbleBit.ClassicTumbler.Client.CLI
 				{
 					Logs.Configuration.LogError(ex.Message);
 					Logs.Configuration.LogDebug(ex.StackTrace);
+				}
+			}
+		}
+		public async Task RedeemEscrows(TumblerClientRuntime runtime)
+		{
+			Logs.Client.LogInformation("Rescanning redeems");
+			var rpc = ((RPCBlockExplorerService)runtime.Services.BlockExplorerService).RPCClient;
+			var rate = await runtime.Services.FeeService.GetFeeRateAsync();
+			foreach(var unspent in await rpc.ListUnspentAsync(0, 99999999))
+			{
+				foreach(var record in runtime.Tracker.Search(unspent.ScriptPubKey))
+				{
+					if(record.TransactionType == Services.TransactionType.ClientRedeem)
+					{
+						Logs.Client.LogInformation("Redeem found " + record.TransactionId);
+						var cycle = runtime.TumblerParameters.CycleGenerator.GetCycle(record.Cycle);
+						var machineState = runtime.CreateStateMachineJob().GetPaymentStateMachineState(cycle);
+						var solver = new PaymentStateMachine(runtime, machineState).SolverClientSession;
+						var broadcast = solver.CreateRedeemTransaction(rate);
+						runtime.Services.TrustedBroadcastService.Broadcast(cycle.Start, Services.TransactionType.ClientRedeem, new CorrelationId(solver.Id), broadcast);
+					}
 				}
 			}
 		}
