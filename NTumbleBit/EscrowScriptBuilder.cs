2083cabe4f013e1d9fba71311f2b24ea00544cdb
nicolas.dorier <nicolas.dorier@gmail.com>
2018-10-14 22:43:40 +0900

    Indent

--- NTumbleBit/EscrowScriptBuilder.cs
+++ NTumbleBit/EscrowScriptBuilder.cs
@@ -84,7 +84,7 @@ namespace NTumbleBit
 			ops.Add(OpcodeType.OP_ELSE);
 			{
 				ops.Add(Op.GetPushOp(LockTime));
-                ops.Add(OpcodeType.OP_CHECKLOCKTIMEVERIFY);
+				ops.Add(OpcodeType.OP_CHECKLOCKTIMEVERIFY);
 				ops.Add(OpcodeType.OP_DROP);
 			}
 			ops.Add(OpcodeType.OP_ENDIF);
@@ -98,8 +98,8 @@ namespace NTumbleBit
 			ops.Add(OpcodeType.OP_ELSE);
 			{
 				ops.Add(Op.GetPushOp(LockTime));
-                ops.Add(OpcodeType.OP_CHECKLOCKTIMEVERIFY);
-                ops.Add(OpcodeType.OP_DROP);
+				ops.Add(OpcodeType.OP_CHECKLOCKTIMEVERIFY);
+				ops.Add(OpcodeType.OP_DROP);
 			}
 			ops.Add(OpcodeType.OP_ENDIF);
 			ops.Add(OpcodeType.OP_CHECKSIG);
